eab6f7147dcc4013f38a6996bc8c82c3c752b9c2
DavidStewartLDN <davidstewart.work@gmail.com>
2020-04-13 15:25:05 +0100

    Lesson 5 End, completed database creation and gitignored database file

--- database/chatbot_database.py
+++ database/chatbot_database.py
@@ -9,6 +9,10 @@ timeframe= '2015-01'
 # You want to insert by doing one large transaction. Should increase speed.
 sql_transaction = []
 
+# Defined for cleanup
+start_row = 0
+cleanup = 1000000
+
 # names databse after timeframe variable.
 connection = sqlite3.connect('{}.db'.format(timeframe))
 c = connection.cursor()
@@ -22,18 +26,6 @@ comment TEXT, subreddit TEXT, unix INT, score INT) """)
 def format_data(data):
   data = data.replace("\n", " newlinechar ").replace("\r"," newlinechar ").replace('"',"'")
   return data
-
-def find_existing_score(parent_id):
-  try:
-    sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
-    c.execute(sql)
-    result = c.fetchone()
-    if result != None:
-      return result[0]
-    else: return False
-  except Exception as e:
-    # print("find_parent", e)
-    return False
 
 # create out acceptance criteria. Data cleaning
 def acceptable(data):
@@ -49,7 +41,6 @@ def acceptable(data):
   else:
     return True
 
-
 def find_parent(pid):
   try:
     sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
@@ -61,6 +52,53 @@ def find_parent(pid):
   except Exception as e:
     # print("find_parent", e)
     return False
+
+def find_existing_score(parent_id):
+  try:
+    sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+    c.execute(sql)
+    result = c.fetchone()
+    if result != None:
+      return result[0]
+    else: return False
+  except Exception as e:
+    # print("find_parent", e)
+    return False
+
+# builds queue of SQL commands so that they are ran at the same time - increases speed!
+def transaction_bldr(sql):
+  global sql_transaction
+  sql_transaction.append(sql)
+  if len(sql_transaction) > 1000:
+    c.execute('BEGIN TRANSACTION')
+    for s in sql_transaction:
+      try:
+        c.execute(s)
+      except:
+        pass
+    connection.commit()
+    sql_transaction = []
+
+def sql_insert_replace_comment(commentid,parentid,parent,comment,subreddit,time,score):
+    try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ? WHERE parent_id =?;""".format(parentid, commentid, parent, comment, subreddit, int(time), score, parentid)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s-UPDATE insertion',str(e))
+
+def sql_insert_has_parent(commentid,parentid,parent,comment,subreddit,time,score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}","{}",{},{});""".format(parentid, commentid, parent, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s-PARENT insertion',str(e))
+
+def sql_insert_no_parent(commentid,parentid,comment,subreddit,time,score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}",{},{});""".format(parentid, commentid, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s-NOPARENT insertion',str(e))
 
 if __name__ == "__main__":
   create_table()
@@ -84,9 +122,31 @@ if __name__ == "__main__":
       parent_data = find_parent(parent_id)
 
       if score >= 2:
-        # Checks if the current comment connected to the parent has a lower sc ore than current comment
-        existing_comment_score = find_existing_score(parent_id)
-        if existing_comment_score:
-          # if score is higher, we will replace it
-          if score > existing_comment_score
-
+        # Check if current comment fits criteria else don't do other computations.
+        if acceptable(body):
+          # Checks if the current comment connected to the parent has a lower sc ore than current comment
+          existing_comment_score = find_existing_score(parent_id)
+          if existing_comment_score:
+            # if score is higher, we will replace it
+            if score > existing_comment_score:
+              sql_insert_replace_comment(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+          
+          else:
+            if acceptable(body):
+              if parent_data:
+                sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+                paired_rows +=1
+              else:
+                sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
+      
+      if row_counter % 100000 == 0:
+        print("Total rows read: {}, Paired rows: {}, Time: {}".format(row_counter, paired_rows, str(datetime.now())))
+      
+      if row_counter > start_row:
+                if row_counter % cleanup == 0:
+                    print("Cleanin up!")
+                    sql = "DELETE FROM parent_reply WHERE parent IS NULL"
+                    c.execute(sql)
+                    connection.commit()
+                    c.execute("VACUUM")
+                    connection.commit()
---
+++ database/create_training_data.py
@@ -0,0 +1,6 @@
+import sqlite3
+import pandas as pd
+
+timeframes = ['2015-01']
+
+for timeframe 
